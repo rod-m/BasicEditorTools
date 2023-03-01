66149271131587e0dbd4aa5f1bc6c36fa7327cee
Rod Martin <r.martin1@salford.ac.uk>
2023-03-01 13:17:58 +0000

    Imporove UI

--- Assets/WorkFlowTools/Editor/QuantizerCreator.cs
+++ Assets/WorkFlowTools/Editor/QuantizerCreator.cs
@@ -9,38 +9,47 @@ namespace WorkFlowTools.Editor
         static public GameObject beatSource;
         static public List<GameObject> beatPrefabs = new List<GameObject>();
 
-        public int numBeats = 4;
-        public int barNumber = 0;
+        private int numBeats = 4;
+        private int barNumber = 0;
+        private int beatVSpace = 2;
         [MenuItem("My Tools/Make Beat")]
         private static void ShowWindow()
+        {
+            MakePrefabs();
+            var window = GetWindow<QuantizerCreator>();
+            window.titleContent = new GUIContent("Make Beats");
+            window.Show();
+        }
+
+        static void MakePrefabs()
         {
             var beatSources = AssetDatabase.FindAssets("l:Beat");
             beatPrefabs = new List<GameObject>();
             foreach (var guid in beatSources)
             {
-               
                 var b = AssetDatabase.GUIDToAssetPath(guid);
                 Debug.Log($"prefab {b}");
                 var p = AssetDatabase.LoadAssetAtPath<GameObject>(b);
                 beatPrefabs.Add(p);
             }
-            var window = GetWindow<QuantizerCreator>();
-            window.titleContent = new GUIContent("Make Beats");
-            window.Show();
         }
 
         private void OnGUI()
         {
             GUILayout.Label("Use this tool to make quantized prefabs");
             EditorGUILayout.BeginHorizontal();
-             beatSource = EditorGUILayout.ObjectField(beatSource, typeof(GameObject), false) as GameObject;
-             EditorGUILayout.EndHorizontal();
-             EditorGUILayout.BeginHorizontal();
+            //TagField
+            beatSource = EditorGUILayout.ObjectField(beatSource, typeof(GameObject), false) as GameObject;
+            GUILayout.Label("Vertical Space", GUILayout.Width(90));
+            beatVSpace = (int)EditorGUILayout.Slider(beatVSpace, 1, 6);
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.BeginHorizontal();
             foreach (var beatPrefab in beatPrefabs)
             {
                 // var z = EditorGUILayout.ObjectField(beatPrefab, typeof(GameObject), false) as GameObject;
                 if (GUILayout.Button($"{beatPrefab.name}")) ChoosePrefab(beatPrefab);
             }
+
             EditorGUILayout.EndHorizontal();
             GUILayout.BeginHorizontal();
             GUILayout.Label("Number of Beats", GUILayout.Width(115));
@@ -48,14 +57,14 @@ namespace WorkFlowTools.Editor
             GUILayout.Label("Bar Number", GUILayout.Width(115));
             barNumber = (int)EditorGUILayout.Slider(barNumber, 0, 4);
             GUILayout.EndHorizontal();
-            
+
             GUILayout.BeginVertical();
             for (int i = 0; i < 4; i++)
             {
                 GUILayout.BeginHorizontal();
                 for (int j = 0; j < numBeats; j++)
                 {
-                    if (GUILayout.Button($"B{i}-{j}")) MakeBeat(i,j);
+                    if (GUILayout.Button($"B{i}-{j}")) MakeBeat(i, j);
                 }
 
                 GUILayout.EndHorizontal();
@@ -68,35 +77,39 @@ namespace WorkFlowTools.Editor
         {
             beatSource = prefab;
         }
+
         void MakeBeat(int i, int j)
         {
-            Debug.Log($"make a beat {barNumber} {i} {j}");
+            //Debug.Log($"make a beat {barNumber} {i} {j}");
             GameObject parent = null;
             GameObject[] go = Selection.gameObjects;
             if (go.Length > 0)
             {
                 parent = go[0];
             }
-            if (beatSource != null)
-            {
-                string beatName = $"Beat-{barNumber}-{i}-{j}";
-                GameObject instance = GameObject.Find(beatName);
-                if (instance == null)
-                {
-                    instance = Instantiate(beatSource) as GameObject;
-                }
-                if (parent != null)
-                {
-                    instance.transform.parent = parent.transform;
-                }
 
-                int beatSpace = 2;
-                instance.name = beatName;
-                float xScale = 4.0f / (float)numBeats;
-                //instance.transform.localScale = new Vector3(xScale,1 ,1);
-                instance.transform.localPosition = new Vector3((j* beatSpace) + (barNumber * numBeats * beatSpace) + xScale/2, -i * beatSpace, 0);
-                
+            if (beatSource == null)
+            {
+                EditorUtility.DisplayDialog("Select beat", "Click a beat option!", "Ok");
+                return;
             }
+
+            string beatName = $"Beat-{barNumber}-{i}-{j}";
+            GameObject instance = GameObject.Find(beatName);
+            if (instance != null)
+            {
+                DestroyImmediate(instance);
+                return;
+            }
+            
+            instance = Instantiate(beatSource) as GameObject;
+            if (parent != null)
+            {
+                instance.transform.parent = parent.transform;
+            }
+
+            instance.name = beatName;
+            instance.transform.localPosition = new Vector3((j * (1 + barNumber) * numBeats), -i * beatVSpace, 0);
         }
     }
 }
